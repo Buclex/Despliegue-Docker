cb5d244af1857cb916a128aa7a1dc990232b1fef
Raul Alberto Niño <raul.nino@est.iudigital.edu.co>
2025-06-09 21:18:49 -0500

    streamlit7

--- src/edu_pad/streamlit_pandas.py
+++ src/edu_pad/streamlit_pandas.py
@@ -1,10 +1,7 @@
 import streamlit as st
 import pandas as pd
-# from streamlit_pandas_profiling import st_profile_report
 from ydata_profiling import ProfileReport
 import os
-
-
 
 
 def main():
@@ -14,18 +11,10 @@ def main():
     df_2 = df[columnas]
     profile = ProfileReport(df_2, title="Dashboard Indicador Dolar")
     st.title("Análisis de Datos")
-    #st.write(profile.to_html(),unsafe_allow
     st.components.v1.html(profile.to_html(), height=600, scrolling=True)
 
 
 
 
-
-    # df = pd.read_csv("src/edu_pad/static/csv/data_extractor.csv")
-    # pr = df.profile_report()
-    # st_profile_report(pr)
-
-
 if __name__ == "__main__":
-    # st.set_page_config(page_title="Dashborad Fianciero", page_icon="🎈", layout="wide")
     main()
